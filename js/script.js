fb429dbb2db2d2083612e26a37e5da02cee50956
Francisco Rotundo <francorotundo90@gmail.com>
2022-08-22 15:30:02 -0400

    Added mobile menu

--- js/script.js
+++ js/script.js
@@ -2,15 +2,28 @@ const email = document.querySelector(".navbar-email");
 const emailMenu = document.querySelector(".desktop-menu");
 const car = document.querySelector(".navbar-shopping-cart");
 const navOrder = document.querySelector(".nav-product-detail");
+const burgerMenu = document.querySelector(".fa-bars");
+const mobileMenu = document.querySelector(".mobile-menu");
 
-email.addEventListener("click", toggleEmailMenu);
-car.addEventListener("click", toggleCarMenu);
+// burgerMenu.addEventListener("click", toggleMobileMenu);
+// email.addEventListener("click", toggleEmailMenu);
+// car.addEventListener("click", toggleCarMenu);
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive");
+    navOrder.classList.add("inactive");
+}
+burgerMenu.addEventListener("click", toggleMobileMenu);
+
 function toggleEmailMenu() {
     emailMenu.classList.toggle("inactive");
     navOrder.classList.add("inactive");
 }
+email.addEventListener("click", toggleEmailMenu);
 
 function toggleCarMenu() {
     navOrder.classList.toggle("inactive");
     emailMenu.classList.add("inactive");
+    mobileMenu.classList.add("inactive");
 }
+car.addEventListener("click", toggleCarMenu);
