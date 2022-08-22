6a4256d09baa47c8b9c9d0c328408c3b619f3228
Francisco Rotundo <francorotundo90@gmail.com>
2022-08-22 11:04:54 -0400

    Added Shopping Cart Menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 const email = document.querySelector(".navbar-email");
 const emailMenu = document.querySelector(".desktop-menu");
+const car = document.querySelector(".navbar-shopping-cart");
+const navOrder = document.querySelector(".nav-product-detail");
 
 email.addEventListener("click", toggleEmailMenu);
+car.addEventListener("click", toggleCarMenu);
 function toggleEmailMenu() {
     emailMenu.classList.toggle("inactive");
+    navOrder.classList.add("inactive");
+}
+
+function toggleCarMenu() {
+    navOrder.classList.toggle("inactive");
+    emailMenu.classList.add("inactive");
 }
